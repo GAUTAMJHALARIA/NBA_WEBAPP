936847b57adb7bb97c15631c131c4af67a0502b2
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-06-27 19:21:41 +0530

    created side bar

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import streamlit as st
 import pandas as pd
+
+st.sidebar.title("NBA Analysis")
+user_menu = st.sidebar.radio(
+    'Select an Option',
+    ('Trends', 'Player Statistics', 'Team Statistics', 'Player Comparison', 'Team Comparison')
+)
