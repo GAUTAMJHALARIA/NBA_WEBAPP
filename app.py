66d2473b4320b8552e9cfc2d53f8ba2a0e5ba66a
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-06-27 19:41:33 +0530

    implemented Preprocessor function on the dataset

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
+from Preprocessing import Preprocessor
 
 st.sidebar.title("NBA Analysis")
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Trends', 'Player Statistics', 'Team Statistics', 'Player Comparison', 'Team Comparison')
 )
+
+# reading the dataset
+df = pd.read_csv("NBA Player Stats(1950 - 2022).csv")
+# Cleaning the Data
+df = Preprocessor(df)
