fd6db974aa6e84d51fe40fc74c65cee729d4724a
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-07-27 00:27:52 +0530

    implemented Preprocessor function on the dataset

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 import streamlit as st
 import pandas as pd
-from Preprocessing import Preprocessor
 
 st.sidebar.title("NBA Analysis")
 user_menu = st.sidebar.radio(
     'Select an Option',
-    ('Trends', 'Player Statistics', 'Team Statistics', 'Player Comparison', 'Team Comparison')
+    ('Player Statistics', 'Team Statistics')
 )
 
-# reading the dataset
-df = pd.read_csv("NBA Player Stats(1950 - 2022).csv")
-# Cleaning the Data
-df = Preprocessor(df)
-per_mode = ["Totals", "Per Game", "Per 36 Mins", "Per 48 Mins"]
-if user_menu == 'Trends':
-    st.sidebar.header("Trends")
-    per_mode = st.sidebar.selectbox("Select Mode", per_mode)
-
-    if per_mode == "Totals":
-        st.title("Totals Stats")
