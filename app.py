4b934668eb24cd3ac648c3e0912a14193feadc95
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-08-02 22:46:20 +0530

    implementing about player section

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 import numpy as np
 import seaborn as sns
 
+from NBA_DATA import get_players_name_list
+from helper import get_player_img
+
 st.sidebar.title("NBA Analysis")
 user_menu = st.sidebar.radio(
     'Select An Option',
@@ -12,17 +15,22 @@ user_menu = st.sidebar.radio(
 
 if user_menu == 'Player-wise':
     col1, col2 = st.columns(2)
-
     with col1:
         with st.container():
             st.header("Player About")
-            st.image(
-                image,
-                width=200,
-                channels="RGB"
-            )
-            st.selectbox("players", players_list)
-            image = get_player_img(2544)
+            selected_player = st.selectbox("Select Player", get_players_name_list())
+            image = get_player_img(selected_player)
+            c1,c2 = st.columns(2)
+            with c1:
+                st.image(
+                    image,
+                    width =200,
+                    channels="RGB"
+                )
+            with c2:
+                pass
+
+
 
     with col2:
         with st.container():
