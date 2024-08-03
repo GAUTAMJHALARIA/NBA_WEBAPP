5207d7b74fc4f1022e4dae2ec06325e8bc4de674
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-08-03 23:36:10 +0530

    implemented get_player_age,get_player_country,get_player_position,get_player_school

--- NBA_DATA.py
+++ NBA_DATA.py
@@ -1,5 +1,6 @@
 from nba_api.stats.static import players
-
+from datetime import datetime
+from nba_api.stats.endpoints import commonplayerinfo
 
 def get_players_name_list():
     players_info = players.get_players()
@@ -12,3 +13,34 @@ def get_id(player_name):
     players_info = players.get_players()
     player_id = [player["id"] for player in players_info if player["full_name"] == player_name]
     return player_id[0]
+
+def get_player_age(player_name):
+    player_id = get_id(player_name)
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
+    birth_date_str = player_info['resultSets'][0]['rowSet'][0][7]  # Birthdate is typically at index 7
+    birth_date = datetime.strptime(birth_date_str[0:10], '%Y-%m-%d')
+
+    today = datetime.today()
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+    return age
+
+def get_player_country(player_name):
+    player_id = get_id(player_name)
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
+    country = player_info['resultSets'][0]['rowSet'][0][9]
+
+    return country
+
+def get_player_position(player_name):
+    player_id = get_id(player_name)
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
+    position = player_info['resultSets'][0]['rowSet'][0][15]
+
+    return position
+
+def get_player_school(player_name):
+    player_id = get_id(player_name)
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
+    school = player_info['resultSets'][0]['rowSet'][0][8]
+    return school
