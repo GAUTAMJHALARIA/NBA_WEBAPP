b282271348195ee467881d2a8df63ede6677553f
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-08-03 19:25:30 +0530

    changed the name format for player image

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def get_player_img(player_name):
     player_id = get_id(player_name)
     url = "https://cdn.nba.com/headshots/nba/latest/1040x760/{}.png".format(player_id)
-    img_file = str(player_id) + ".png"
+    img_file = str(player_name) + ".png"
     pic = urlretrieve(url, img_file)
     player_img = plt.imread(pic[0])
     return player_img
