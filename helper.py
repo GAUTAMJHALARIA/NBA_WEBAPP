f98d0c7bbadb139cb67d8a965b9c3655053de994
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2024-08-04 22:31:00 +0530

    implemented draw_court function

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 from NBA_DATA import get_id
 from urllib.request import urlretrieve
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Rectangle, Arc
 
 
 def get_player_img(player_name):
@@ -12,5 +13,55 @@ def get_player_img(player_name):
     return player_img
 
 
+def draw_court(ax=None, color="black", lw=1, outer_lines=False):
+    if ax is None:
+        ax = plt.gca()
 
+    # Basketball Hoop
+    hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
 
+    # backboard
+    backboard = Rectangle((-30, -12.5), 60, 0, linewidth=lw, color=color)
+
+    # The paint
+    # outer box
+    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color, fill=False)
+    # inner box
+    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color, fill=False)
+
+    # Free Throw Top Arc
+    top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180, linewidth=lw, color=color, fill=False)
+
+    # Free Bottom Top Arc
+    bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0, linewidth=lw, color=color)
+
+    # Restricted Zone
+    restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw, color=color)
+
+    # Three Point Line
+    corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw, color=color)
+    corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
+    three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw, color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0, linewidth=lw, color=color)
+    center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0, linewidth=lw, color=color)
+
+    # list of court shapes
+    court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw, bottom_free_throw, restricted,
+                      corner_three_a, corner_three_b, three_arc, center_outer_arc, center_inner_arc]
+
+    # outer_lines=True
+    if outer_lines:
+        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw, color=color, fill=False)
+        court_elements.append(outer_lines)
+
+    for element in court_elements:
+        ax.add_patch(element)
+
+    court_elements = [hoop]
+
+    for element in court_elements:
+        ax.add_patch(element)
+
+    return ax
